2417d28ad9899f8d5f8b996ba2d6b25ebb3b36c6
Christopher Tate <ctate@google.com>
2020-05-28 17:49:53 -0700

    DO NOT MERGE - Only autoVerify at install for new hosts
    Re-run app link verification at update time only when the set of hosts
    has expanded.  Intentionally revoke verify history when an app stops
    using autoVerify, as a one-time measure to place it back into the
    non-autoverify model for tracking the user's launch preferences.  If the
    app starts using autoVerify again later, it behaves identically to an
    app that has never done so before.
    
    Bug: 151475497
    Bug: 146204120
    Test: described on master CL
    
    Merged-In: I200d85085ce79842a3ed39377d1f75ec381c8991
    Merged-In: Ibaf087946966ad82d60c7b255e3ee75990716b63
    (cherry picked from commit 90b716a442802154208c86c31545b6f1f3ee53db)
    
    [basilgello: Backport to 14.1:
     - idleController.addPowerSaveTempWhitelistApp does not exist,
     - domains is ArrayList<String> not ArraySet<String> so adding
       dummy domainsList]
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>
    
    Change-Id: Ibaf087946966ad82d60c7b255e3ee75990716b63

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -1321,7 +1321,8 @@ final class Settings {
         return result;
     }
 
-    boolean removeIntentFilterVerificationLPw(String packageName, int userId) {
+    boolean removeIntentFilterVerificationLPw(String packageName, int userId,
+            boolean alsoResetStatus) {
         PackageSetting ps = mPackages.get(packageName);
         if (ps == null) {
             if (DEBUG_DOMAIN_VERIFICATION) {
@@ -1329,14 +1330,17 @@ final class Settings {
             }
             return false;
         }
-        ps.clearDomainVerificationStatusForUser(userId);
+        if (alsoResetStatus) {
+            ps.clearDomainVerificationStatusForUser(userId);
+        }
+        ps.setIntentFilterVerificationInfo(null);
         return true;
     }
 
     boolean removeIntentFilterVerificationLPw(String packageName, int[] userIds) {
         boolean result = false;
         for (int userId : userIds) {
-            result |= removeIntentFilterVerificationLPw(packageName, userId);
+            result |= removeIntentFilterVerificationLPw(packageName, userId, true);
         }
         return result;
     }
