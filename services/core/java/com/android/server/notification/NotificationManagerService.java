b5ba89664f993cc43e7f80938364804d28baf563
Julia Reynolds <juliacr@google.com>
2025-03-03 06:28:05 -0800

    Revert "Check sound Uri permission when creating a notification channel"
    This reverts commit 665e1aee2dbad19d436a08d753b4d5cf733abeb5.
    
    Reason for revert: b/337775777
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:2d9506053e521542fc6b8127f44c9d76897904c1)
    Merged-In: Ib569f25a616eb13439be177901fdb78fa02f539a
    Change-Id: Ib569f25a616eb13439be177901fdb78fa02f539a

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2260,7 +2260,6 @@ public class NotificationManagerService extends SystemService {
                 mZenModeHelper,
                 new NotificationChannelLoggerImpl(),
                 mAppOps,
-                mUgmInternal,
                 new SysUiStatsEvent.BuilderFactory());
         mRankingHelper = new RankingHelper(getContext(),
                 mRankingHandler,
@@ -5565,7 +5564,13 @@ public class NotificationManagerService extends SystemService {
             final Uri originalSoundUri =
                     (originalChannel != null) ? originalChannel.getSound() : null;
             if (soundUri != null && !Objects.equals(originalSoundUri, soundUri)) {
-                PermissionHelper.grantUriPermission(mUgmInternal, soundUri, sourceUid);
+                Binder.withCleanCallingIdentity(() -> {
+                    mUgmInternal.checkGrantUriPermission(sourceUid, null,
+                            ContentProvider.getUriWithoutUserId(soundUri),
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION,
+                            ContentProvider.getUserIdFromUri(soundUri,
+                            UserHandle.getUserId(sourceUid)));
+                });
             }
         }
 
--- services/core/java/com/android/server/notification/PreferencesHelper.java
+++ services/core/java/com/android/server/notification/PreferencesHelper.java
@@ -68,7 +68,6 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.util.Preconditions;
 import com.android.internal.util.XmlUtils;
-import com.android.server.uri.UriGrantsManagerInternal;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -174,7 +173,6 @@ public class PreferencesHelper implements RankingConfig {
     private final ZenModeHelper mZenModeHelper;
     private final NotificationChannelLogger mNotificationChannelLogger;
     private final AppOpsManager mAppOps;
-    private final UriGrantsManagerInternal mUgmInternal;
 
     private SparseBooleanArray mBadgingEnabled;
     private SparseBooleanArray mBubblesEnabled;
@@ -193,7 +191,6 @@ public class PreferencesHelper implements RankingConfig {
     public PreferencesHelper(Context context, PackageManager pm, RankingHandler rankingHandler,
             ZenModeHelper zenHelper, NotificationChannelLogger notificationChannelLogger,
             AppOpsManager appOpsManager,
-            UriGrantsManagerInternal ugmInternal,
             SysUiStatsEvent.BuilderFactory statsEventBuilderFactory) {
         mContext = context;
         mZenModeHelper = zenHelper;
@@ -202,7 +199,6 @@ public class PreferencesHelper implements RankingConfig {
         mNotificationChannelLogger = notificationChannelLogger;
         mAppOps = appOpsManager;
         mStatsEventBuilderFactory = statsEventBuilderFactory;
-        mUgmInternal = ugmInternal;
 
         updateBadgingEnabled();
         updateBubblesEnabled();
@@ -958,12 +954,6 @@ public class PreferencesHelper implements RankingConfig {
                         : NotificationChannel.DEFAULT_ALLOW_BUBBLE);
             }
             clearLockedFieldsLocked(channel);
-
-            // Verify that the app has permission to read the sound Uri
-            // Only check for new channels, as regular apps can only set sound
-            // before creating. See: {@link NotificationChannel#setSound}
-            PermissionHelper.grantUriPermission(mUgmInternal, channel.getSound(), uid);
-
             channel.setImportanceLockedByOEM(r.oemLockedImportance);
             if (!channel.isImportanceLockedByOEM()) {
                 if (r.oemLockedChannels.contains(channel.getId())) {
