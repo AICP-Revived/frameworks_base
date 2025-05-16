ba1df4318ce7f98cbabb636c5882392144efeb6f
Riddle Hsu <riddlehsu@google.com>
2025-05-16 11:59:46 +0800

    Do not allow custom animation of untrusted Toast
    Since target API level 30 (Android 11), regular text toast is shown
    by SystemUI (See#CHANGE_TEXT_TOASTS_IN_THE_SYSTEM). While legacy
    apps and deprecated custom view show the toast in its own processes,
    so force the default animation style for these usages to avoid
    unexpected animation duration.
    
    Such when setForceHideNonSystemOverlayWindowIfNeeded is triggered,
    the regular Toast displayed by SystemUI won't be affected (because
    SystemUI has internal permission), and the untrusted Toast can be
    hidden in a short time (config_longAnimTime=500ms) instead of a
    customized duration.
    
    Bug: 408215749
    Flag: EXEMPT bugfix
    Test: Show a Toast with modified
          WindowManager.LayoutParams#windowAnimations
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:aeca871e495b6f6481fb445cb74d8afaaa8d2376)
    Merged-In: Ib2844dedebfa3ec9fc8333af3f66bb07cd4d3d7f
    Change-Id: Ib2844dedebfa3ec9fc8333af3f66bb07cd4d3d7f
    
    Change-Id: I9ff35187b3a9a91e4a1aeef39e86332114c7c8af

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -955,6 +955,10 @@ public class DisplayPolicy {
                 attrs.hideTimeoutMilliseconds = mAccessibilityManager.getRecommendedTimeoutMillis(
                         (int) attrs.hideTimeoutMilliseconds,
                         AccessibilityManager.FLAG_CONTENT_TEXT);
+                // Do not allow untrusted toast to customize animation.
+                if (!win.mSession.mCanAddInternalSystemWindow) {
+                    attrs.windowAnimations = R.style.Animation_Toast;
+                }
 
                 switch(Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.TOAST_ANIMATION, 1, UserHandle.USER_CURRENT)) {
