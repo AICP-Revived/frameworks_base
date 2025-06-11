yaap/frameworks_base_old

base: Allow toggling QS footer text [1/2]
Change-Id: If765a1de25141de568d0dc7b8f1be1f1308fd4e6

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -84,5 +84,6 @@ public class SystemSettings {
         Settings.System.DISPLAY_COLOR_MODE,
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
+        Settings.System.QS_FOOTER_TEXT_SHOW,
     };
 }

AICP-Revived/frameworks_base

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -158,7 +158,8 @@ public class SystemSettings {
                 Settings.System.STATUS_BAR_LOGO_COLOR,
                 Settings.System.STATUS_BAR_LOGO_POSITION,
                 Settings.System.STATUS_BAR_LOGO_STYLE,
-                Settings.System.STATUS_BAR_LOGO_COLOR_ACCENT
+                Settings.System.STATUS_BAR_LOGO_COLOR_ACCENT,
+                Settings.System.QS_FOOTER_TEXT_SHOW
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);