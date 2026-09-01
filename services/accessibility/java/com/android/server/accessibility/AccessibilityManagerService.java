HaloUI-AOSP/android_frameworks_base

[BACKPORT] Include all enabled services when FEEDBACK_ALL_MASK.
Bug: 243849844
Test: m sts;
      sts-tradefed run sts-dynamic-develop -m CtsAccessibilityTestCases
Change-Id: I4f93e06d1066085bd64e8f09882de2f4a72a0633
(cherry picked from commit 2bc4d49c2b0265f5de1c62d1342b1426cc5e1377)
Merged-In: I4f93e06d1066085bd64e8f09882de2f4a72a0633

services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -592,7 +592,8 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
                 final Service service = services.get(i);
                 // Don't report the UIAutomation (fake service)
                 if (!sFakeAccessibilityServiceComponentName.equals(service.mComponentName)
-                        && (service.mFeedbackType & feedbackType) != 0) {
+                        && (service.mFeedbackType & feedbackType) != 0
+                            || feedbackType == AccessibilityServiceInfo.FEEDBACK_ALL_MASK) {
                     result.add(service.mAccessibilityServiceInfo);
                 }
             }

AICP-Revived/frameworks_base

services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -529,7 +529,8 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
                     Service service = services.get(i);
                     // Don't report the UIAutomation (fake service)
                     if (!sFakeAccessibilityServiceComponentName.equals(service.mComponentName)
-                            && (service.mFeedbackType & feedbackTypeBit) != 0) {
+                            && (service.mFeedbackType & feedbackTypeBit) != 0
+                                || feedbackType == AccessibilityServiceInfo.FEEDBACK_ALL_MASK) {
                         result.add(service.mAccessibilityServiceInfo);
                     }
                 }