msft-mirror-aosp/platform.frameworks.base

Use setHideOverlayWindows instead of AppOpsManager
... to hide non-system overlay windows

Previously, OverlayControl use AppOpsManager to control overlay show and hide.
However it didn't stop overlay window showing again without
SYSTEM_ALERT_WINDOW permission when the restrictions are released.

This fix use Window#setHideOverlayWindows mechanism instead to prevent
any non-system overlay window shown on top of the window.

Test: presubmit
Bug: 463364901
Flag: EXEMPT bugfix
Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:740cb9e8f2e4bafac209c865b3b304b0b4731d00
Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:62526d34ef2420ff73b2def704e4487323c5496e
Merged-In: Id566caf9b1c39fd202dbbc3376cb2e7c48a4eac7
Change-Id: Id566caf9b1c39fd202dbbc3376cb2e7c48a4eac7

services/autofill/java/com/android/server/autofill/ui/SaveUi.java
@@ -161,8 +161,6 @@ final class SaveUi {
 
     private final @NonNull OneActionThenDestroyListener mListener;
 
-    private final @NonNull OverlayControl mOverlayControl;
-
     private final CharSequence mTitle;
     private final CharSequence mSubTitle;
     private final PendingUi mPendingUi;
@@ -180,15 +178,14 @@ final class SaveUi {
            @NonNull CharSequence serviceLabel, @NonNull Drawable serviceIcon,
            @Nullable String servicePackageName, @NonNull ComponentName componentName,
            @NonNull SaveInfo info, @NonNull ValueFinder valueFinder,
-           @NonNull OverlayControl overlayControl, @NonNull OnSaveListener listener,
-           boolean nightMode, boolean isUpdate, boolean compatMode, boolean showServiceIcon) {
+           @NonNull OnSaveListener listener, boolean nightMode, boolean isUpdate,
+           boolean compatMode, boolean showServiceIcon) {
         if (sVerbose) {
             Slogf.v(TAG, "nightMode: %b displayId: %d", nightMode, context.getDisplayId());
         }
         mThemeId = nightMode ? THEME_ID_DARK : THEME_ID_LIGHT;
         mPendingUi = pendingUi;
         mListener = new OneActionThenDestroyListener(listener);
-        mOverlayControl = overlayControl;
         mServicePackageName = servicePackageName;
         mComponentName = componentName;
         mCompatMode = compatMode;
@@ -364,6 +361,7 @@ final class SaveUi {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         window.setGravity(Gravity.BOTTOM | Gravity.CENTER);
         window.setCloseOnTouchOutside(true);
+        window.setHideOverlayWindows(true);
         final WindowManager.LayoutParams params = window.getAttributes();
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -645,16 +643,11 @@ final class SaveUi {
     private void show() {
         Slog.i(TAG, "Showing save dialog: " + mTitle);
         mDialog.show();
-        mOverlayControl.hideOverlays();
-   }
+    }
 
     PendingUi hide() {
         if (sVerbose) Slog.v(TAG, "Hiding save dialog.");
-        try {
-            mDialog.hide();
-        } finally {
-            mOverlayControl.showOverlays();
-        }
+        mDialog.hide();
         return mPendingUi;
     }
 
@@ -663,16 +656,12 @@ final class SaveUi {
     }
 
     void destroy() {
-        try {
-            if (sDebug) Slog.d(TAG, "destroy()");
-            throwIfDestroyed();
-            mListener.onDestroy();
-            mHandler.removeCallbacksAndMessages(mListener);
-            mDialog.dismiss();
-            mDestroyed = true;
-        } finally {
-            mOverlayControl.showOverlays();
-        }
+        if (sDebug) Slog.d(TAG, "destroy()");
+        throwIfDestroyed();
+        mListener.onDestroy();
+        mHandler.removeCallbacksAndMessages(mListener);
+        mDialog.dismiss();
+        mDestroyed = true;
     }
 
     private void throwIfDestroyed() {

AICP-Revived/frameworks_base

services/autofill/java/com/android/server/autofill/ui/SaveUi.java
@@ -159,8 +159,6 @@ final class SaveUi {
 
     private final @NonNull OneActionThenDestroyListener mListener;
 
-    private final @NonNull OverlayControl mOverlayControl;
-
     private final CharSequence mTitle;
     private final CharSequence mSubTitle;
     private final PendingUi mPendingUi;
@@ -178,13 +176,12 @@ final class SaveUi {
            @NonNull CharSequence serviceLabel, @NonNull Drawable serviceIcon,
            @Nullable String servicePackageName, @NonNull ComponentName componentName,
            @NonNull SaveInfo info, @NonNull ValueFinder valueFinder,
-           @NonNull OverlayControl overlayControl, @NonNull OnSaveListener listener,
-           boolean nightMode, boolean isUpdate, boolean compatMode) {
+           @NonNull OnSaveListener listener, boolean nightMode,
+           boolean isUpdate, boolean compatMode) {
         if (sVerbose) Slog.v(TAG, "nightMode: " + nightMode);
         mThemeId = nightMode ? THEME_ID_DARK : THEME_ID_LIGHT;
         mPendingUi = pendingUi;
         mListener = new OneActionThenDestroyListener(listener);
-        mOverlayControl = overlayControl;
         mServicePackageName = servicePackageName;
         mComponentName = componentName;
         mCompatMode = compatMode;
@@ -356,6 +353,7 @@ final class SaveUi {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         window.setGravity(Gravity.BOTTOM | Gravity.CENTER);
         window.setCloseOnTouchOutside(true);
+        window.setHideOverlayWindows(true);
         final WindowManager.LayoutParams params = window.getAttributes();
         params.width = WindowManager.LayoutParams.MATCH_PARENT;
         params.accessibilityTitle = context.getString(R.string.autofill_save_accessibility_title);
@@ -647,16 +645,11 @@ final class SaveUi {
     private void show() {
         Slog.i(TAG, "Showing save dialog: " + mTitle);
         mDialog.show();
-        mOverlayControl.hideOverlays();
-   }
+    }
 
     PendingUi hide() {
         if (sVerbose) Slog.v(TAG, "Hiding save dialog.");
-        try {
-            mDialog.hide();
-        } finally {
-            mOverlayControl.showOverlays();
-        }
+        mDialog.hide();
         return mPendingUi;
     }
 
@@ -665,16 +658,12 @@ final class SaveUi {
     }
 
     void destroy() {
-        try {
-            if (sDebug) Slog.d(TAG, "destroy()");
-            throwIfDestroyed();
-            mListener.onDestroy();
-            mHandler.removeCallbacksAndMessages(mListener);
-            mDialog.dismiss();
-            mDestroyed = true;
-        } finally {
-            mOverlayControl.showOverlays();
-        }
+        if (sDebug) Slog.d(TAG, "destroy()");
+        throwIfDestroyed();
+        mListener.onDestroy();
+        mHandler.removeCallbacksAndMessages(mListener);
+        mDialog.dismiss();
+        mDestroyed = true;
     }
 
     private void throwIfDestroyed() {